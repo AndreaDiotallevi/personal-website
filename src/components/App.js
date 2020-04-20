e3774e41011d2041d1b7860e35e35ee7eae004cb
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-04-20 11:29:09 +0100

    Add ProjectList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ProjectList from "./ProjectList";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <ProjectList />
+    </div>
+  );
 };
 
 export default App;

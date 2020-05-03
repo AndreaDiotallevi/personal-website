554f66df0b87def204570dd657194b4718138b01
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-03 12:55:20 +0100

    Add routes.js util

---
+++ src/utils/routes.js
@@ -0,0 +1,15 @@
+const routes = [
+  {
+    menuName: "Dev Portfolio",
+    baseUrl: "/portfolio",
+    pageTitle: "",
+  },
+  { menuName: "Contact", baseUrl: "/contact", pageTitle: "" },
+  {
+    menuName: "Home",
+    baseUrl: "/",
+    pageTitle: "Software Engineer and Generative Artist",
+  },
+];
+
+export default routes;

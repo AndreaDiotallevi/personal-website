f91df8ae0260ce82a9f25fcc8fd730e3a12d9dc2
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-03 12:52:58 +0100

    Amend ProjectGif component

--- src/components/ProjectGif.js
+++ src/components/ProjectGif.js
@@ -4,7 +4,7 @@ const ProjectGif = ({ project }) => {
   return (
     <div className="project-gif-component">
       <video controls loop muted className="project-video">
-        <source src={require(`../assets/${project.videoUrl}`)} />
+        <source src={require(`../../assets/${project.videoUrl}`)} />
       </video>
     </div>
   );

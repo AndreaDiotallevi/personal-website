fd6081d4024b74a0e3ae4ebcd6c966e4be892f54
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-03 12:51:20 +0100

    Amend Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,10 @@
 import React from "react";
-import Moonlight from "./Moonlight";
+import Moonlight from "../Moonlight/Moonlight";
 
-const Home = () => {
+const Home = (props) => {
   return (
-    <div className="home-component">
-      <div className="home-container">
-        <div className="home-page-text">
-          <h3 className="home-page-title">Hello! My name is Andrea.</h3>
-          <p className="home-page-paragraph" id="home-page-paragraph-1">
-            I am a software engineer, architect and pianist, fascinated by the
-            intersection between art and technology.
-          </p>
-          <p className="home-page-paragraph" id="home-page-paragraph-2">
-            For each new work, I design a custom algorithm capable of generating
-            a sequence of unique, but aesthetically related images.
-          </p>
-        </div>
+    <div id="home-component">
+      <div id="home-container">
         <Moonlight />
       </div>
     </div>

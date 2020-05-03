ebff6efb9b110ebbfaa021c52153ffba0c44d71a
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-03 12:51:43 +0100

    Amend Moonlight component

--- src/components/Moonlight.js
+++ src/components/Moonlight.js
@@ -5,38 +5,44 @@ class Moonlight extends React.Component {
   t = 0;
 
   setup = (p5, canvasParentRef) => {
-    const height = document.getElementsByClassName("home-container")[0]
-      .offsetHeight;
-    console.log(height);
-    p5.createCanvas(p5.windowWidth, height).parent(canvasParentRef);
+    const height = document.getElementById("home-container").offsetHeight;
+    const width = document.getElementById("home-container").offsetWidth;
+    p5.createCanvas(width, height).parent(canvasParentRef);
     p5.background(56, 68, 76);
   };
 
   draw = (p5) => {
-    const height = document.getElementsByClassName("home-container")[0]
-      .offsetHeight;
-    p5.translate(p5.windowWidth / 2, height / 2);
-    const w = p5.min(p5.windowWidth, height);
-    const n = p5.noise;
+    if (this.t > 0) {
+      const height = document.getElementById("home-container").offsetHeight;
+      const width = document.getElementById("home-container").offsetWidth;
 
-    const a = 0.49 * w;
-    const b = n(this.t) * 6;
-    const c = n(this.t++ + 60) * 6;
+      p5.translate(width / 2, height / 2);
+      const w = p5.min(width, height);
+      const n = p5.noise;
 
-    p5.noFill();
-    p5.stroke(255, 10);
-    p5.line(p5.cos(b) * a, p5.sin(b) * a, p5.cos(c) * a, p5.sin(c) * a);
+      const a = 0.48 * w;
+      let b = n(this.t) * 6;
+      let c = n(this.t + 60) * 6;
+
+      p5.noFill();
+      p5.stroke(255, 25);
+      p5.line(p5.cos(b) * a, p5.sin(b) * a, p5.cos(c) * a, p5.sin(c) * a);
+    }
+
+    this.t++;
   };
 
   mouseClicked = (p5) => {
-    p5.noiseSeed(p5.random());
+    p5.noiseSeed(p5.random(1000));
     p5.background(56, 68, 76);
     this.t = 0;
     p5.draw();
   };
 
   windowResized = (p5) => {
-    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    const height = document.getElementById("home-container").offsetHeight;
+    const width = document.getElementById("home-container").offsetWidth;
+    p5.resizeCanvas(width, height);
     p5.noiseSeed(p5.random());
     p5.background(56, 68, 76);
     this.t = 0;

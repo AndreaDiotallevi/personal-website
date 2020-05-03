e0d887316f4fe1056b5714e59f408203b83bee55
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-03 12:50:11 +0100

    Amend App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,60 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Navbar from "./Navbar";
-import PageTitle from "./PageTitle";
-import Home from "./Home";
-import ProjectList from "./ProjectList";
-import Contact from "./Contact";
 
-const App = () => {
-  return (
-    <div className="app-component">
-      <BrowserRouter>
-        <Route component={Navbar}></Route>
-        <Route component={PageTitle}></Route>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/dev" component={ProjectList}></Route>
-        <Route exact path="/contact" component={Contact}></Route>
-      </BrowserRouter>
-    </div>
-  );
-};
+import Navbar from "../Navbar/Navbar";
+import PageTitle from "../PageTitle/PageTitle";
+import Home from "../Home/Home";
+import ProjectList from "../ProjectList/ProjectList";
+import Contact from "../Contact/Contact";
+import Footer from "../Footer/Footer";
+import Menu from "../Menu/Menu";
+// import Blog from "../Blog/Blog";
+
+class App extends React.Component {
+  state = {
+    menuOpen: false,
+  };
+
+  handleMenuClick() {
+    this.setState({ menuOpen: !this.state.menuOpen });
+  }
+
+  handleLinkClick() {
+    this.setState({ menuOpen: false });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <BrowserRouter>
+          <Route
+            render={(routeProps) => (
+              <Navbar
+                {...routeProps}
+                open={this.state.menuOpen}
+                onClick={() => this.handleMenuClick()}
+              />
+            )}
+          />
+          <Route
+            render={(routeProps) => (
+              <Menu
+                {...routeProps}
+                open={this.state.menuOpen}
+                onClick={() => this.handleLinkClick()}
+              />
+            )}
+          />
+          <Route component={PageTitle} />
+          <Route path="/" exact component={Home} />
+          <Route path="/portfolio" exact component={ProjectList} />
+          <Route path="/contact" exact component={Contact} />
+          {/* <Route path="/blog" exact component={Blog} /> */}
+          <Route component={Footer} />
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;

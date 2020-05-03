bc887cd484d2a5514e48e0fd07ce4161a38471a1
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-03 12:52:05 +0100

    Amend Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,68 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MenuToggleButton from "../MenuToggleButton/MenuToggleButton";
+import routes from "../../utils/routes";
 
-const Navbar = () => {
+const Navbar = ({ open, onClick, history }) => {
+  const pathname = history.location.pathname;
+
+  const getLinkClassName = (url) => {
+    let classes = "";
+
+    if (pathname === "/") {
+      classes += " is-home";
+    }
+
+    if (pathname.includes(url) && url !== "/") {
+      classes += " link-active";
+    } else if (pathname === "/" && url === "/") {
+      classes += " link-active";
+    }
+
+    return classes;
+  };
+
+  const getComponentClassName = () => {
+    if (history.location.pathname === "/") {
+      return "is-home-component";
+    }
+  };
+
   return (
-    <div className="navbar-component">
-      <div className="navbar-container">
+    <div
+      id="navbar-component"
+      className={`navbar-component ${getComponentClassName()}`}
+    >
+      <div id="navbar-container">
         <div className="navbar-left">
           <Link to="/">
-            <h1 className="navbar-title">Andrea Diotallevi</h1>
+            <h1
+              id="navbar-title"
+              className={`navbar-title ${getLinkClassName()}`}
+            >
+              ANDREA DIOTALLEVI
+            </h1>
           </Link>
         </div>
         <div className="navbar-right">
+          {routes.map((route) => (
+            <Link
+              to={route.baseUrl}
+              className={`navbar-link ${getLinkClassName(route.baseUrl)}`}
+              key={route.menuName}
+            >
+              {route.menuName}
+            </Link>
+          ))}
           <a
             href="https://medium.com/@andreadiotallevi"
             target="_blank"
             rel="noopener noreferrer"
-            className="navbar-anchor"
+            className={`navbar-link ${getLinkClassName()}`}
           >
             Blog
           </a>
-          <Link to="/" className="navbar-link">
-            <p>Dev</p>
-          </Link>
-          <Link to="/contact" className="navbar-link">
-            <p>Contact</p>
-          </Link>
+          <MenuToggleButton open={open} onClick={onClick} history={history} />
         </div>
       </div>
     </div>

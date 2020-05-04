4ae6406cc31bc52ebd7d5f428d89509244d206d2
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-04 17:22:55 +0100

    Amend ProjectOverview component

--- src/components/ProjectOverview.js
+++ src/components/ProjectOverview.js
@@ -1,40 +1,22 @@
 import React from "react";
 
 const ProjectOverview = ({ project }) => {
-  const renderApis = () => {
-    if (project.apis !== null) {
-      return (
-        <span>
-          <p className="margin-left">
-            <strong>External APIs: </strong>
-            {project.apis}
-          </p>
-        </span>
-      );
-    }
-  };
-
   return (
     <div className="project-overview-component">
-      <h3 className="project-name">{project.name}</h3>
+      <h1 className="project-name">{project.name}</h1>
       <p className="project-overview">{project.overview}</p>
+      <p>Built with {project.devTechnologies}</p>
       <p>
-        <strong>Stack: </strong>
-        {project.devTechnologies}
-      </p>
-      {renderApis()}
-      <p className="margin-left">
-        <strong>Testing, CI, CD: </strong>
-        {project.testingTechnologies}
-      </p>
-      <div className="project-anchors-container">
+        Check out the{" "}
         <a href={project.demoUrl} target="_blank" rel="noopener noreferrer">
-          Live Demo
-        </a>
+          Live demo
+        </a>{" "}
+        and the{" "}
         <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">
-          GitHub Repo
+          GitHub code
         </a>
-      </div>
+        .
+      </p>
     </div>
   );
 };

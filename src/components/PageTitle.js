88a514fa01d422fb161fc8dfee9f57f2f2d4fa52
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-04 17:22:18 +0100

    Amend PageTitle component

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -4,18 +4,6 @@ import routes from "../utils/routes";
 const PageTitle = ({ history }) => {
   const pathname = history.location.pathname;
 
-  const getComponentClassName = () => {
-    if (pathname === "/") {
-      return "is-home-component";
-    }
-  };
-
-  const getDescriptionClassName = () => {
-    if (pathname === "/") {
-      return "is-home-description";
-    }
-  };
-
   const getPageTitle = () => {
     const baseUrl = "/" + pathname.split("/")[1];
     const route = routes.filter((route) => route.baseUrl === baseUrl)[0];
@@ -23,11 +11,9 @@ const PageTitle = ({ history }) => {
   };
 
   return (
-    <div className={`page-title-component ${getComponentClassName()}`}>
+    <div className="page-title-component">
       <div className="page-title-container">
-        <h2 className={`page-title ${getDescriptionClassName()}`}>
-          {getPageTitle()}
-        </h2>
+        <h2 className="page-title">{getPageTitle()}</h2>
       </div>
     </div>
   );

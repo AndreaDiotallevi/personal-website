9cd2bd6e4df924291140c6985725337ef666e534
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-25 18:06:18 +0100

    Add spinner to iframe when loading

--- src/components/ProjectIFrame.js
+++ src/components/ProjectIFrame.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProjectIFrame = ({ project }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const hideSpinner = () => {
+    console.log("here loaded");
+    setIsLoading(false);
+  };
+
   return (
     <div className="project-iframe-component">
       <div className="device-border">
         <div className="responsive">
-          <iframe src={project.demoUrl} title={project.name}></iframe>
+          {isLoading ? <div className="loading"></div> : null}
+          <iframe
+            src={project.demoUrl}
+            title={project.name}
+            onLoad={hideSpinner}
+          ></iframe>
         </div>
       </div>
     </div>
